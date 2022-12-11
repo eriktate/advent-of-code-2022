e32966078b30ecde9ca29f94da59a6837d95e3c0
Erik Tate <erik@truebill.com>
2022-12-11 10:33:39 -0500

    adding day6 solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+// const SEQUENCE_LEN: usize = 4; // part one
+const SEQUENCE_LEN: usize = 14; // part two
+
+fn append(window: &mut [char; SEQUENCE_LEN], ch: char) {
+    for idx in 1..SEQUENCE_LEN {
+        window[idx - 1] = window[idx];
+    }
+
+    window[SEQUENCE_LEN - 1] = ch;
+}
+
+fn part_one(input: &str) -> usize {
+    let mut window = ['\0'; SEQUENCE_LEN];
+
+    for (idx, ch) in input.chars().enumerate() {
+        append(&mut window, ch);
+        let set: HashSet<char> = HashSet::from_iter(window.iter().cloned());
+        if set.len() == SEQUENCE_LEN && idx >= SEQUENCE_LEN {
+            return idx + 1;
+        }
+    }
+
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+
+    println!("part one: {}", part_one(input));
 }
